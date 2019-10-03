809dc18632cb18685f54ffabcaed57353e18acec
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-03 19:57:23 +0200

    (too) simplistic shell script and a base class

---
+++ High/KittyHigh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitty {
+    abstract class KittyHigh {
+        public abstract void Show(string src);
+        public void Show(StringBuilder src) => Show(src);
+    }
+
+
+    class KittyPL:KittyHigh {
+        protected List<string> KeyWords;
+    }
+}

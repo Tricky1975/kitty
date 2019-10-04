3a72bff888d4a12404ef03fa1515c568a9756251
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-04 20:20:52 +0200

    Assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kitty")]
-[assembly: AssemblyCopyright("Copyright Ã‚Â©  2019")]
+[assembly: AssemblyCopyright("Copyright (c) Jeroen P. Broks  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

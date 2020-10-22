6be5102b79df2d7c9c57f2149a45a343067cdb5a
Jeroen Broks <facebook@tbbs.nl>
2020-10-22 15:23:42 +0200

    Start encoding issue

--- High/KittyHighNeil.cs
+++ High/KittyHighNeil.cs
@@ -17,7 +17,7 @@
 // misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
-???namespace Kitty {
+namespace Kitty {
     class KittyHighNeil : KittyPL {
         public KittyHighNeil() {
             caseinsensitive = true;
